027746c921cc46cc80166055e3c497a2fc605eb6
Yacow Vircowskiy <vcow@yandex.ru>
2018-04-15 21:05:21 +0300

    ai

--- project/Assets/Scripts/Controllers/AI/Enemy.cs
+++ project/Assets/Scripts/Controllers/AI/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using Models;
+﻿using System;
+using Models;
 using Properties;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -14,8 +15,39 @@ namespace Controllers.AI
             {
                 return (CardType) ((int) Mathf.Round(Random.value * 2));
             }
-            
-            return CardType.Paper;
+
+            const float deadHeatProb = 1.0f / 3.0f * 0.5f; // Вероятность выпадения ничьей.
+            var userWinProb = (1.0f - deadHeatProb) * Mathf.Clamp01(model.LuckPercent);
+            var userLoseProb = Mathf.Clamp01(1.0f - (deadHeatProb + userWinProb));
+
+            var rnd = Random.value;
+            if (rnd <= deadHeatProb)
+            {
+                // Ничья
+                return model.UserCard;
+            }
+            else if (rnd <= deadHeatProb + userLoseProb)
+            {
+                // Юзер проигрывает
+                switch (model.UserCard)
+                {
+                    case CardType.Paper: return CardType.Scissors;
+                    case CardType.Scissors: return CardType.Stone;
+                    case CardType.Stone: return CardType.Paper;
+                }
+            }
+            else
+            {
+                // Юзер побеждает
+                switch (model.UserCard)
+                {
+                    case CardType.Paper: return CardType.Stone;
+                    case CardType.Scissors: return CardType.Paper;
+                    case CardType.Stone: return CardType.Scissors;
+                }
+            }
+
+            throw new NotSupportedException();
         }
     }
 }
